0f638596f3ca0bd5cacf2466d345def1fb80ea7c
张景锋 <734546749@qq.com>
2019-09-16 11:45:34 +0800

    打包

---
+++ build/precache-manifest.e6baf1687aea17b8887c97c1239995ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2f2cca1a2f9bd97b8f88",
+    "url": "/static/js/main.2f2cca1a.chunk.js"
+  },
+  {
+    "revision": "1dac3c21521775f77fe3",
+    "url": "/static/js/1.1dac3c21.chunk.js"
+  },
+  {
+    "revision": "2f2cca1a2f9bd97b8f88",
+    "url": "/static/css/main.01a19ba9.chunk.css"
+  },
+  {
+    "revision": "1dac3c21521775f77fe3",
+    "url": "/static/css/1.281a451d.chunk.css"
+  },
+  {
+    "revision": "2e6f95fd234f010baf02776e65ecefe6",
+    "url": "/index.html"
+  }
+];
